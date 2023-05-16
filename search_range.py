aae3cfee882110b25aefbe788d2e6352c354a910
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-05-16 08:08:22 +0330

    search_insert

---
+++ search_range.py
@@ -0,0 +1,28 @@
+def search_range(nums: list, target: int) -> list:
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if target > nums[mid]:
+            low = mid + 1
+        elif target < nums[mid]:
+            high = mid - 1
+        else:
+            break
+
+    start = mid
+    end = mid
+
+    while True:
+        if nums[start-1] == target:
+            start -= 1
+        if nums[end+1] == target:
+            end += 1
+
+        if nums[start - 1] and nums[end + 1] != target:
+            break
+    return [start, end]
+
+
+print(search_range([1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 5, 5, 5, 7, 7, 7, 9, 9, ], 4))
